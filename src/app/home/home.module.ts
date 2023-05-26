ee0cadea773194b161e0e3ceba627618fe927ad1
victoramattosc <victorcarbelotti0306@gmail.com>
2023-05-26 12:26:53 -0300

    Carousel feito!

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { CarouselComponent } from '../components/carousel/carousel.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -6,15 +7,15 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, CarouselComponent]
 })
 export class HomePageModule {}
 
